1d41d0992664f3e96f77e5a1bcfab8671000132f
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-04-18 14:59:02 +0700

    add: login screen

--- lib/auth/login_screen.dart
+++ lib/auth/login_screen.dart
@@ -1,13 +1,188 @@
+import 'package:antdesign_icons/antdesign_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Login Screen'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 80, left: 15, right: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Image(
+              image: AssetImage('images/login_vector.png'),
+              width: 175,
+            ),
+            const Padding(padding: EdgeInsets.symmetric(vertical: 14)),
+            Text(
+              'Silahkan masuk dengan nomor telkomsel kamu',
+              style: GoogleFonts.poppins(
+                textStyle: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xFF1E272E),
+                ),
+              ),
+            ),
+            const Padding(padding: EdgeInsets.symmetric(vertical: 14)),
+            Text(
+              'Nomor HP',
+              style: GoogleFonts.poppins(
+                textStyle: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xFF1E272E),
+                ),
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 15),
+                hintText: 'Cth. 08129011xxxx',
+                hintStyle: GoogleFonts.poppins(
+                    textStyle: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xFFA4B0BE),
+                )),
+                border: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  borderSide: BorderSide(color: Color(0xFFCED6E0)),
+                ),
+                enabledBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  borderSide: BorderSide(color: Color(0xFFCED6E0)),
+                ),
+              ),
+            ),
+            const Padding(padding: EdgeInsets.symmetric(vertical: 4)),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Checkbox(
+                  checkColor: Colors.white,
+                  fillColor: MaterialStateProperty.all(const Color(0xFFEC2028)),
+                  value: isChecked,
+                  onChanged: (value) {
+                    setState(() {
+                      isChecked = value!;
+                    });
+                  },
+                ),
+                Expanded(
+                  child: Text(
+                    'Saya menyetujui syarat, ketentuan, dan privasi Telkomsel',
+                    style: GoogleFonts.poppins(
+                      textStyle: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xFF1E272E),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Padding(padding: EdgeInsets.symmetric(vertical: 6)),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(40.0),
+                backgroundColor: const Color(0xFFEC2028),
+              ),
+              onPressed: () {},
+              child: Text(
+                'Lanjut'.toUpperCase(),
+                style: GoogleFonts.poppins(
+                  textStyle: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            const Padding(padding: EdgeInsets.symmetric(vertical: 6)),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Text(
+                'Atau masuk menggunakan',
+                style: GoogleFonts.poppins(
+                  textStyle: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF747D8C),
+                  ),
+                ),
+              )
+            ]),
+            const Padding(padding: EdgeInsets.symmetric(vertical: 6)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: OutlinedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(
+                      AntIcons.facebookFilled,
+                      color: Color(0xFF3B5998),
+                    ),
+                    label: Text(
+                      'Facebook',
+                      style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            color: Color(0xFF3B5998)),
+                      ),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(color: Color(0xFF3B5998)),
+                    )
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                ),
+                Expanded(
+                  child: OutlinedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(
+                      AntIcons.twitterSquareFilled,
+                      color: Color(0xFF1DA1F2),
+                    ),
+                    label: Text(
+                      'Twitter',
+                      style: GoogleFonts.poppins(
+                        textStyle: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            color: Color(0xFF1DA1F2)),
+                      ),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                      side: const BorderSide(color: Color(0xFF1DA1F2)),
+                    )
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
