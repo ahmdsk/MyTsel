a91f1a04deab7f64679568fd9414935edb8da2e4
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-04-19 06:58:28 +0700

    add: bottom navigation in home screen

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,10 +10,42 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
-      ),
-    );
+    return Scaffold(
+        body: ListView(
+          children: [
+            Stack(
+              children: const [
+                Text('test dua tigas'),
+                Text('test1')
+              ],
+            )
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: const Color(0xFFEC2028),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: 'Beranda',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.watch_later_outlined),
+              label: 'Riwayat',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.help_outline_rounded),
+              label: 'Bantuan',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.mail_outline_rounded),
+              label: 'Inbox',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              label: 'Akun Saya',
+            ),
+          ],
+        ));
   }
 }
